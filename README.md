056b207c4e6febdef699b5fd88a29e282eda20fe
Gregor Adams <greg@pixelass.com>
2016-08-12 00:24:31 +0200

    fix(gitignore): added index.js
    index.js was ignored

--- README.md
+++ README.md
@@ -1,5 +1,20 @@
 # jsx-create-element
 
+[![GitHub license](https://img.shields.io/github/license/pixelass/jsx-create-element.svg)](https://github.com/pixelass/jsx-create-element/blob/master/LICENSE)  
+[![Travis](https://img.shields.io/travis/pixelass/jsx-create-element.svg)](https://travis-ci.org/pixelass/jsx-create-element)
+[![David](https://img.shields.io/david/pixelass/jsx-create-element.svg)](https://david-dm.org/pixelass/jsx-create-element)
+[![David](https://img.shields.io/david/dev/pixelass/jsx-create-element.svg)](https://david-dm.org/pixelass/chrome-extension-boilerplate#info=devDependencies&view=table)  
+[![GitHub issues](https://img.shields.io/github/issues/pixelass/jsx-create-element.svg)](https://github.com/pixelass/jsx-create-element/issues)
+[![GitHub forks](https://img.shields.io/github/forks/pixelass/jsx-create-element.svg)](https://github.com/pixelass/jsx-create-element/network)
+[![GitHub stars](https://img.shields.io/github/stars/pixelass/jsx-create-element.svg)](https://github.com/pixelass/jsx-create-element/stargazers)  
+[![js-standard-style](https://img.shields.io/badge/code%20style-standard-brightgreen.svg)](http://standardjs.com/)
+[![Standard Version](https://img.shields.io/badge/release-standard%20version-brightgreen.svg)](https://github.com/conventional-changelog/standard-version)
+[![Commitizen friendly](https://img.shields.io/badge/commitizen-friendly-brightgreen.svg)](http://commitizen.github.io/cz-cli/)
+
+## Demo
+ [Demo on Codepen](http://codepen.io/pixelass/pen/wWRRrB)
+ 
+## Usage
 
 ```shell
 npm install --save-dev jsx-create-element
@@ -19,3 +34,4 @@ const hello = name => (
 
 jsx.render(hello('World'), document.body)
 ```
+
---
+++ src/index.js
@@ -0,0 +1,89 @@
+import HTML_TAGS from './html-tags'
+import GLOBAL_ATTRIBUTES from './global-attributes'
+import EVENT_HANDLERS from './event-handlers'
+
+/**
+ * render content inside an element
+ * @param  {DOMNode} content - HTML content to render
+ * @param  {DOMNode} target - target element
+ */
+const render = (content, target) => {
+  // append content to target
+  target.appendChild(content)
+}
+
+/**
+ * create an element and assign attributes and handlers
+ * also appends childNodes
+ * @param  {String} tagName - defines which tagType to render
+ * @param  {Object} [props] - a list of properties containing attributes and eventlisteners
+ * @param  {...[type]} childNodes - a collection of childNodes to append
+ * @return {DOMNode} returns an HTML element
+ */
+const createElement = (tagName, properties = {}, ...childNodes) => {
+  // make sure props is an object
+  const props = properties || {}
+
+  if (!(tagName in HTML_TAGS)) {
+    return
+  }
+  // get the tagname
+  const tag = HTML_TAGS[tagName]
+  // tag could be an object
+  // use a flag to check when needed
+  const object = typeof tag === 'object'
+  // define local attributes and extend the global attributes
+  const localAttrs = object ? tag.attributes || {} : {}
+  const attrs = Object.assign({}, GLOBAL_ATTRIBUTES, localAttrs)
+  const tagType = object ? tag.name : tag
+
+  // create the element
+  const el = document.createElement(tagType)
+
+  // loop through all props and assign attributes and eventlisteners
+  Object.keys(props).forEach(prop => {
+    if (prop in attrs) {
+      el.setAttribute(attrs[prop], props[prop])
+    } else if (prop in EVENT_HANDLERS) {
+      el.addEventListener(EVENT_HANDLERS[prop], props[prop])
+    }
+  })
+
+  // if props contains a style object
+  // set styles on element
+  if ('style' in props) {
+    const styles = props.style
+
+    Object.keys(styles).forEach(prop => {
+      const value = styles[prop]
+      // make numbers default to px
+      if (typeof value === 'number') {
+        el.style[prop] = `${value}px`
+      } else if (typeof value === 'string') {
+        el.style[prop] = value
+      } else {
+        throw new Error(`Expected "number" or "string" but received "${typeof value}"`)
+      }
+    })
+  }
+
+  // loop through childNodes and append.
+  // if string create a textNode
+  childNodes.forEach(childNode => {
+
+    if (typeof childNode === 'object') {
+      el.appendChild(childNode)
+    } else if (typeof childNode === 'string') {
+      el.appendChild(document.createTextNode(childNode))
+    } else {
+      throw new Error(`Expected "object" or "string" but received "${typeof value}"`)
+    }
+  })
+
+  return el
+}
+
+export default {
+  render,
+  createElement
+}
